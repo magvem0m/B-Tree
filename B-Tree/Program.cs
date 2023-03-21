957e48f4e1cd6cfb47be2cc343d4bce41a0be329
Tesold <73257835+Tesold@users.noreply.github.com>
2023-03-21 21:40:35 +0300

    private class

--- B-Tree/Program.cs
+++ B-Tree/Program.cs
@@ -1,4 +1,4 @@
-﻿BTree<string> bTree = new();
+﻿BTree<string> bTree = new(); 
 
 bTree.Add("5");
 bTree.Add("4");
@@ -16,29 +16,27 @@ bTree.DepthFirst();
 
 class BTree<T> where T: IComparable
 {
-    private BTreeNode<T>? _Root;
-    public BTreeNode<T>? Root => _Root;
-
+    private BTreeNode? Root;
 
     public void Add(T value)
     {
         if (Root == null)
         {
-            _Root = new BTreeNode<T>(value, null);
+            Root = new BTreeNode(value, null);
             return;
         }
 
         Add(value, Root);
     }
 
-    private void Add(T value, BTreeNode<T> currentNode)
+    private void Add(T value, BTreeNode currentNode)
     {
         int compared = value.CompareTo(currentNode.Value);
 
         switch (compared)
         {
-            case <= 0: if (currentNode.Left == null) currentNode.Left = new BTreeNode<T>(value, currentNode); else Add(value, currentNode.Left); break;
-            case > 0: if (currentNode.Right == null) currentNode.Right = new BTreeNode<T>(value, currentNode); else Add(value, currentNode.Right); break;
+            case <= 0: if (currentNode.Left == null) currentNode.Left = new BTreeNode(value, currentNode); else Add(value, currentNode.Left); break;
+            case > 0: if (currentNode.Right == null) currentNode.Right = new BTreeNode(value, currentNode); else Add(value, currentNode.Right); break;
         }
     }
 
@@ -54,12 +52,12 @@ class BTree<T> where T: IComparable
         }
 
 
-        Queue<BTreeNode<T>> queue = new();
+        Queue<BTreeNode> queue = new();
         queue.Enqueue(Root);
 
         while(queue.Count > 0)
         {
-            BTreeNode<T> node = queue.Dequeue();
+            BTreeNode node = queue.Dequeue();
 
             if (node.Left != null)
                 queue.Enqueue(node.Left);
@@ -79,7 +77,7 @@ class BTree<T> where T: IComparable
         DepthFirst(Root);
     }
 
-    private void DepthFirst(BTreeNode<T> node)
+    private void DepthFirst(BTreeNode node)
     {
         Console.WriteLine("Value: " + node.Value);
 
@@ -92,23 +90,23 @@ class BTree<T> where T: IComparable
         if (node.Right != null)
             DepthFirst(node.Right);
     }
-}
 
-class BTreeNode<T> where T : IComparable
-{
-    private BTreeNode<T>? _Left, _Right;
-    private readonly BTreeNode<T>? _Parent;
-    private T _Value;
-
-    public BTreeNode(T value, BTreeNode<T>? parent)
+    private class BTreeNode
     {
-        _Value = value;
-        _Parent = parent;
-    }
+        private BTreeNode? _Left, _Right;
+        private readonly BTreeNode? _Parent;
+        private T _Value;
 
-    public BTreeNode<T>? Left { get => _Left; set { _Left = value; } }
-    public BTreeNode<T>? Right { get => _Right; set { _Right = value; } }
-    public BTreeNode<T>? Parent => _Parent;
-    public T Value { get => _Value; set { _Value = value; } }
-    public bool IsList => Left == null && Right == null;
+        public BTreeNode(T value, BTreeNode? parent)
+        {
+            _Value = value;
+            _Parent = parent;
+        }
+
+        public BTreeNode? Left { get => _Left; set { _Left = value; } }
+        public BTreeNode? Right { get => _Right; set { _Right = value; } }
+        public BTreeNode? Parent => _Parent;
+        public T Value { get => _Value; set { _Value = value; } }
+        public bool IsList => Left == null && Right == null;
+    }
 }
